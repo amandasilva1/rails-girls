b3f6784ffd4510d842a3015d33ca70d83a2b26f0
amanda da silva <amandatecmeioamb@gmail.com>
2017-12-09 11:46:53 -0500

    Fix heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
-Rails.application.routes.draw do
-  get 'pages/info'
+Rails.application.routes.draw do 
+	root to: redirect('/ideas')
+    get 'pages/info'
 
-  resources :ideas
+     resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
